828cdf81517c0a35ff659ec3b6417c2da3491993
Szymek <szymon.rzadca@gmail.com>
2018-07-28 10:47:33 +0200

    another example of if statements

--- Part5.js
+++ Part5.js
@@ -1,15 +1,32 @@
-var hot = false
-var temp = 50
+// var hot = false
+// var temp = 10
+//
+// if (temp>80) {
+//   console.log("Hot Outside!");
+// }
+// else if (temp <= 80 && temp >= 50) {
+//   console.log("Average temp outside");
+// }
+// else if (temp < 50 && temp >= 32) {
+//   console.log("It's pretty cold out!");
+// }
+// else {
+//   console.log("It is very cold out");
+// }
 
-if (temp>80) {
-  console.log("Hot Outside!");
+var ham = 0;
+var cheese = 0;
+
+var report = "blank";
+
+if (ham >= 10 && cheese >= 10) {
+  report = "Strong sales of both ham and cheese!"
 }
-else if (temp <= 80 && temp >= 50) {
-  console.log("Average temp outside");
-}
-else if (temp < 50 && temp >= 32) {
-  console.log("It's pretty cold out!");
+else if (ham === 0 && cheese === 0) {
+  report = "Nothing Sold!"
 }
 else {
-  console.log("It is very cold out");
+  report = "We had some sales of items"
 }
+
+console.log(report);
