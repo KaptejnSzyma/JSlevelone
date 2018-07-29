3375c30f5f3334fae285d1992dc75b0064ff8d70
Szymek <szymon.rzadca@gmail.com>
2018-07-29 10:32:48 +0200

    adding some control flow

--- Part6.js
+++ Part6.js
@@ -2,7 +2,12 @@ var x = 0;
 
 while (x < 5) {
   console.log("x is currently: " + x);
-  console.log("x is still less than 5, adding 1 to x");
-}
 
-x = x+1;
+  if (x===3) {
+    console.log("X IS EQUAL TO THREE!");
+    break;
+  }
+
+  console.log("x is still less than 5, adding 1 to x");
+  x= x+1;
+}
